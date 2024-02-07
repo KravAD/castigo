ef8049146504a20e1eff16af3165b6fb2085629e
KravAD <dkrav@myuax.com>
2024-02-07 18:22:59 +0100

    commit 5

--- ejercicio2.java
+++ ejercicio2.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        char maxChar = Character.MAX_VALUE;
+        short maxShort = Short.MAX_VALUE;
+        int maxInt = Integer.MAX_VALUE;
+
+        System.out.println("El mayor número entero que se puede representar con un char es: " + (int)maxChar);
+        System.out.println("El mayor número entero que se puede representar con un short es: " + maxShort);
+        System.out.println("El mayor número entero que se puede representar con un int es: " + maxInt);
     }
 
 }
